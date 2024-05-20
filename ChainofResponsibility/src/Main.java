3af0b91031c43c879c18a8360c6788e2049deae2
anrodr <160557932+anrodr@users.noreply.github.com>
2024-05-19 22:26:50 -0500

    Update Main.java
    add request

--- ChainofResponsibility/src/Main.java
+++ ChainofResponsibility/src/Main.java
@@ -10,9 +10,11 @@ public class Main {
         SupportRequest request1 = new SupportRequest("Reset password", RequestType.SIMPLE);
         SupportRequest request2 = new SupportRequest("Fix software bug", RequestType.INTERMEDIATE);
         SupportRequest request3 = new SupportRequest("Repair hardware", RequestType.COMPLEX);
+        SupportRequest request4 = new SupportRequest("Fix operating system", RequestType.INTERMEDIATE);
 
         level1.handleRequest(request1);
         level1.handleRequest(request2);
         level1.handleRequest(request3);
+        level1.handleRequest(request4);
     }
 }
